550037bb060ac1f953b4a50a7f0a6763108314a7
Antti Helminen <antti@gsb.fi>
2021-09-05 00:28:57 +0300

    fixed: typo

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ int main(int argc, char *argv[])
 {
     QCoreApplication::setOrganizationName("Breblsoft");
     QCoreApplication::setOrganizationDomain("gsb.fi");
-    QCoreApplication::setApplicationName("pop-up menu");
+    QCoreApplication::setApplicationName("pop-up memo");
 
     QApplication a(argc, argv);
     MainWindow w;
